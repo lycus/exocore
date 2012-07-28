86caf2babb8ee75cdefb17e911d26818a3f1b5a5
alexrp <alex@lycus.org>
2012-07-28 05:45:50 +0200

    Add KERNEL_VMA for 64-bit.

--- src/exocore/asm/boot.s
+++ src/exocore/asm/boot.s
@@ -13,7 +13,12 @@ MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS) ; Fabulous Multiboot
 PAGE_SIZE equ 0x1000 ; Size of memory pages.
 PAGE_DIRECTORY_ENTRIES equ 1024 ; Number of entries in the page directory.
 
+%ifdef EXOCORE_IS_32_BIT
 KERNEL_VMA equ 0xC0000000 ; Keep in sync with the linker script.
+%else
+KERNEL_VMA equ 0xffff800000000000 ; Keep in sync with the linker script.
+%endif
+
 KERNEL_PAGE_NUMBER equ KERNEL_VMA / PAGE_SIZE / PAGE_DIRECTORY_ENTRIES
 
 section .data
