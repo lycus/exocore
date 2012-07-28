87b7ebfda0953e04bb2a1d2dd68612785c083dd1
alexrp <alex@lycus.org>
2012-07-28 08:21:38 +0200

    More 64-bit work (interrupts).

--- src/exocore/asm/interrupts.s
+++ src/exocore/asm/interrupts.s
@@ -88,8 +88,28 @@ extern isr_handler
 align 8
 isr_common_stub:
 
+%ifdef EXOCORE_IS_32_BIT
     ; Save all GPRs.
     pushad
+%else
+    ; Save all GPRs.
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rsp
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endif
 
     ; Back up the data segment descriptor.
     mov ax, ds
@@ -112,8 +132,34 @@ isr_common_stub:
     mov fs, ax
     mov gs, ax
 
-    popad ; Pop all backed-up GPRs.
-    add esp, 8 ; Pop the interrupt identifier and error code.
+%ifdef EXOCORE_IS_32_BIT
+    ; Restore all GPRs.
+    popad
+
+    ; Pop the interrupt identifier and error code.
+    add esp, 4 + 4
+%else
+    ; Restore all GPRs.
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rsp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+
+    ; Pop the interrupt identifier and error code.
+    add esp, 8 + 8
+%endif
 
     ; Enable interrupts.
     sti
@@ -159,8 +205,28 @@ extern irq_handler
 align 8
 irq_common_stub:
 
+%ifdef EXOCORE_IS_32_BIT
     ; Save all GPRs.
     pushad
+%else
+    ; Save all GPRs.
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rsp
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endif
 
     ; Back up the data segment descriptor.
     mov ax, ds
@@ -183,8 +249,34 @@ irq_common_stub:
     mov fs, ax
     mov gs, ax
 
-    popad ; Pop all backed-up GPRs.
-    add esp, 8 ; Pop the interrupt identifier and error code.
+%ifdef EXOCORE_IS_32_BIT
+    ; Restore all GPRs.
+    popad
+
+    ; Pop the interrupt identifier and error code.
+    add esp, 4 + 4
+%else
+    ; Restore all GPRs.
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rsp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+
+    ; Pop the interrupt identifier and error code.
+    add esp, 8 + 8
+%endif
 
     ; Enable interrupts.
     sti
